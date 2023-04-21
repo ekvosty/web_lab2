3559ed837e35b32feb408dfa48acf3983738ce72
ekvosty <128777037+ekvosty@users.noreply.github.com>
2023-04-21 09:40:52 +0700

    Add files via upload
    Создание веб-страницы со слайдером изображений

---
+++ EX2/JSSlider/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoWidth:true,
+    responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:4
+      },
+      2000:{
+          items:5
+      }
+  }
+  });
+});
